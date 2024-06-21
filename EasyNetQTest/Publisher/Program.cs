cc27fdd4a7d02e9e5e38fa558f9f23813bf3022a
Daniil Vilchinskiy <danvilcka@gmail.com>
2024-06-21 17:36:19 +0300

    Create RabbitMQServer by Dockerfile. Testing MessageBus for using in project.

---
+++ EasyNetQTest/Publisher/Program.cs
@@ -0,0 +1,21 @@
+﻿    using System;
+    using EasyNetQ;
+    using Messages;
+
+    namespace Publisher 
+    {
+        class Program 
+        {
+            static void Main(string[] args) 
+            {
+                using var bus = RabbitHutch.CreateBus("host=localhost");
+                var input = String.Empty;
+                Console.WriteLine("Enter a message. 'Quit' to quit.");
+                while ((input = Console.ReadLine()) != "Quit")
+                {
+                    bus.PubSub.Publish(new TextMessage { Text = input ?? "" });
+                    Console.WriteLine("Message published!");
+                }
+            }
+        }
+    }
